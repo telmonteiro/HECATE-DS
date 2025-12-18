23ddd3af83fb2ff66e581ef4296636fd482b00f9
melmotonteiro <telmo.monteiro@ua.pt>
2025-12-18 17:47:21 +0000

    public

---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../src'))
+
+project = 'HECATE'
+copyright = '2025, Telmo Monteiro'
+author = 'Telmo Monteiro'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',  # For Google and NumPy style docstrings
+    'sphinx.ext.viewcode'   # Optional: to include links to source code
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'Python'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
